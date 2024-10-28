e3984d0b454e885265e6adccad93f8fe91ef1010
AsCaillet <cailletayton@gmail.com>
2024-10-28 21:43:34 +0100

    Ajout LCD Screen + début de debug

--- src/main.c
+++ src/main.c
@@ -5,10 +5,31 @@
  */
 
 #include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include "lcd_screen_i2c.h"
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/devicetree.h>
+
+#define LED_YELLOW_NODE DT_ALIAS(led_yellow)
+#define LCD_I2C_NODE DT_NODELABEL(lcd_screen)
+
+const struct i2c_dt_spec dev_lcd_screen = I2C_DT_SPEC_GET(LCD_I2C_NODE);
+const struct gpio_dt_spec led_yellow_gpio = GPIO_DT_SPEC_GET_OR(LED_YELLOW_NODE, gpios, {0});
 
 int main(void)
 {
+	// Init device
+    init_lcd(&dev_lcd_screen);
+
+    // Display a message
+    write_lcd(&dev_lcd_screen, HELLO_MSG, LCD_LINE_1);
+    write_lcd_clear(&dev_lcd_screen, ZEPHYR_MSG, LCD_LINE_2);
+
+	// Hello World
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
 
+	// LED ON
+	gpio_pin_configure_dt(&led_yellow_gpio, GPIO_OUTPUT_HIGH);
 	return 0;
 }
